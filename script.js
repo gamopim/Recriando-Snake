a794f7f5f5663e884c89d88e23b7df0ac3f85fa0
gamopim <39862288+gamopim@users.noreply.github.com>
2021-04-12 19:30:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,15 @@ function iniciarJogo(){
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake [i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
 
     criarBG();
     criarCobrinha();
@@ -69,9 +77,7 @@ function iniciarJogo(){
         
     }
 
-    
-
-    let newHead = { 
+        let newHead = { 
         x: snakeX,
         y: snakeY
     }
